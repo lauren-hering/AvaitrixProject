38f85b0b001d4d2ad4425e97b8948eaef0c1753b
Apple <apple@Apples-MacBook-Pro-141.local>
2019-07-10 14:49:17 -0500

    commit iteration 1

--- Aviatrix.swift
+++ Aviatrix.swift
@@ -12,6 +12,8 @@ class Aviatrix {
     
     var author = ""
     var running = false
+    var data = AviatrixData()
+    var location = "St. Louis"
     
     init (authorName : String) {
         author = authorName
@@ -27,14 +29,15 @@ class Aviatrix {
     }
     
     func flyTo(destination : String) {
+        location = destination
         
     }
     
-    func distanceTo(target : String) {
-    
+    func distanceTo(target : String) -> Int {
+        return data.knownDistances[location]![target]!
     }
     
     func knownDestinations() -> [String] {
-       return ["St. Louis"]
+       return ["St. Louis", "Phoenix", "Denver", "SLC" ]
     }
 }
